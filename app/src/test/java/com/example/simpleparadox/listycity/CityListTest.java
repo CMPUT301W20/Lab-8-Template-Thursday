24bc3e2f8b3b39105cbc6f41e1b859e0529bafb4
Triang1e <35908688+xiyuan1@users.noreply.github.com>
2020-03-13 12:32:41 -0600

    first wrong test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,11 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testFirstProvince(){
+        CityList cityList = mockCityList();
+
+        assertEquals("Edmonton",cityList.getFirstProvinceName());
+    }
 }
